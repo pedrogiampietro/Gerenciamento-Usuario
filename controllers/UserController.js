c6d9eeb2f481e37dc9cbbff05a203cfc6e9819d3
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-05-21 20:55:24 -0300

    Adicionado Controllers

---
+++ controllers/UserController.js
@@ -0,0 +1,82 @@
+class UserController { 
+
+    constructor(formId, tableId){
+
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+
+        this.onSubmit();
+
+    }
+
+
+    onSubmit(){
+
+        this.formEl.addEventListener("submit", event => {
+
+            event.preventDefault();
+
+            this.AddLine(this.getValues());
+
+         });
+    }
+
+
+
+    // criando o metodo para pegar os valores dos filhos e retornar.
+
+    getValues(){
+
+        let user = {};
+
+        this.formEl.elements.forEach(function (field, index) { 
+
+            if (field.name == "gender") { 
+        
+                if (field.checked) {
+                    user[field.name] = field.value;
+                }
+        
+            } else {
+        
+                user [field.name] = field.value;
+        
+            }
+        
+            // console.log(field.name)
+        
+        });
+
+        return new User(
+            user.name, 
+            user.gender, 
+            user.birth, 
+            user.country, 
+            user.email, 
+            user.password, 
+            user.photo, 
+            user.admin
+            );
+
+    }
+
+    AddLine(dataUser){
+    
+        this.tableEl.innerHTML = `
+        
+        <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+      </tr>
+    
+        `;
+    }
+
+}
